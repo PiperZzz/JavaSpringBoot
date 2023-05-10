906d393b3181a9770066ff2b111689c154a7f1dd
Panda77 <PiperZzz@users.noreply.github.com>
2023-05-10 18:04:35 -0400

    Spring Boot Web Server Init

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,27 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Hello World!");
+		logger.info("Hello World!");
+	}
+
+	@RestController
+	public class HelloWorldController {
+		
+		@GetMapping("/")
+		public String index() {
+			return "Hello World";
+		}
 	}
 
 }

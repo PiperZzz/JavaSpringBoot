90df49d693ee22334a2e5cf91814ac75f32c59af
Panda77 <fight4uking@gmail.com>
2023-05-11 06:43:18 -0400

    POST

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -13,15 +19,30 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		logger.info("Hello World!");
+		logger.info("POST Ready");
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+						.allowCredentials(true);
+			}
+		};
 	}
 
 	@RestController
+	@RequestMapping
 	public class HelloWorldController {
-		
-		@GetMapping("/")
-		public String index() {
-			return "Hello World";
+
+		@PostMapping("/submit")
+		public @ResponseBody String submitString(@RequestBody String input) {
+			logger.info("Received: " + input);
+			return input + "!";
 		}
 	}
 

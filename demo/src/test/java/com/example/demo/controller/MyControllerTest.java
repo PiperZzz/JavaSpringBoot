4425d5ce898f250d9591605c7f53face6cacb603
Panda77 <PiperZzz@users.noreply.github.com>
2023-05-12 12:32:59 -0400

    add test

--- demo/src/test/java/com/example/demo/controller/MyControllerTest.java
+++ demo/src/test/java/com/example/demo/controller/MyControllerTest.java
@@ -1,29 +1,24 @@
 package com.example.demo.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class MyControllerTests {
+public class MyControllerTest {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	void submitString() throws Exception {
-		String input = "Hello World";
-		this.mockMvc.perform(post("/mycontroller/submit")
-			.content(input)
-			.contentType(MediaType.TEXT_PLAIN))
-			.andExpect(status().isOk())
-			.andExpect(content().string(input + "!"));
-	}
+    @Test
+    public void testSubmitString() throws Exception {
+        this.mockMvc.perform(post("/submit").content("test"))
+                .andExpect(status().isOk());
+    }
 }

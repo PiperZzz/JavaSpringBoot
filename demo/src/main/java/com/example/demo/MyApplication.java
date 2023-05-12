6347001d755620bb12bb8590eacc7dd4bc56fd77
Panda77 <PiperZzz@users.noreply.github.com>
2023-05-12 10:10:02 -0400

    folder refactored

---
+++ demo/src/main/java/com/example/demo/MyApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class MyApplication {
+	private static final Logger logger = LoggerFactory.getLogger(MyApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+		logger.info("Server Ready!");
+	}
+}

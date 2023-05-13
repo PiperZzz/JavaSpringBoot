4ae35ba11e6f0cbc0eb17760a53e90f9a3bd951d
Panda77 <PiperZzz@users.noreply.github.com>
2023-05-13 07:59:00 -0400

    fix file name

--- demo/src/main/java/com/example/demo/controller/MyController.java
+++ demo/src/main/java/com/example/demo/controller/MyController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 public class MyController {
     private static final Logger logger = LoggerFactory.getLogger(MyController.class);
 
--- demo/src/test/java/com/example/demo/controller/MyControllerTest.java
+++ demo/src/test/java/com/example/demo/controller/MyControllerTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -18,7 +19,11 @@ public class MyControllerTest {
 
     @Test
     public void testSubmitString() throws Exception {
-        this.mockMvc.perform(post("/submit").content("test"))
-                .andExpect(status().isOk());
+        String input = "test";
+        mockMvc.perform(post("/api/submit")
+                .contentType(MediaType.TEXT_PLAIN)
+                .content(input))
+                .andExpect(status().isOk())
+                .andExpect(content().string(input + "!"));
     }
 }

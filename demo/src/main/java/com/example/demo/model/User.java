4a83c26d164ce612ce03abb528c50f681129c668
Panda77 <fight4uking@gmail.com>
2023-05-14 19:15:08 -0400

    lombok for getter and setter

--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -2,11 +2,16 @@ package com.example.demo.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class User {
     @Id
     private Long id;
     private String name;
-    // getters and setters
+    private String email;
+    private String password;
+    private String role;
+    private String status;
 }

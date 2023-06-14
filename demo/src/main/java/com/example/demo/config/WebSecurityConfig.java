293c7c71001eb24c3f854ec25e8dee048a881126
Panda77 <PiperZzz@users.noreply.github.com>
2023-06-14 09:15:13 -0400

    use WebSecurityConfiguration

--- demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,22 +1,26 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/api/login").permitAll()
+public class WebSecurityConfig extends WebSecurityConfiguration {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/api/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().permitAll()
-                .and()
-                .logout().permitAll();
+                .formLogin(login -> login.permitAll())
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
     }
 }

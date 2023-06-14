bc1b275087db254fe59e8fe31bc1fd28b504c396
Panda77 <PiperZzz@users.noreply.github.com>
2023-06-14 11:14:46 -0400

    remove dedendency

--- demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -4,22 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfiguration {
+public class WebSecurityConfig {
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http
+                .csrf().disable()        
+                .authorizeHttpRequests()
                 .requestMatchers("/api/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin(login -> login.permitAll())
-                .logout(LogoutConfigurer::permitAll);
+                .httpBasic();
 
         return http.build();
     }

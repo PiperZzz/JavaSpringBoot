0d87793f8f197d6ca696eb8a363eed1c17a47877
Panda77 <PiperZzz@users.noreply.github.com>
2023-06-19 18:21:44 -0400

    Lambda DSL

--- demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -5,19 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
-    
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()        
-            .authorizeHttpRequests()
-            .requestMatchers("/api/login", "/api/register").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .httpBasic();
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/login", "/api/register").permitAll()
+                .anyRequest().authenticated())
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(withDefaults());
 
         return http.build();
     }

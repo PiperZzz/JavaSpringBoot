a14812560b67822ea27ca1a80b1af014973ad635
Panda77 <PiperZzz@users.noreply.github.com>
2023-07-23 19:34:51 -0400

    Add wallet entity

--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -1,7 +1,10 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import lombok.Data;
@@ -9,9 +12,14 @@ import lombok.Data;
 @Entity
 @Data
 public class User {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @OneToMany(mappedBy = "user")
+    private List<Wallet> wallets;
+
     private String name;
     private String email;
     private String password;
---
+++ demo/src/main/java/com/example/demo/model/Wallet.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import lombok.Data;
+
+@Entity
+@Data
+public class Wallet {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    
+    private String cryptocurrency;
+    private Double balance;
+}

630afd15f40d2dc9a56bc370b4772a58dc113c7a
Panda77 <PiperZzz@users.noreply.github.com>
2023-07-25 19:22:06 -0400

    EnumUtil init

---
+++ demo/src/main/java/com/example/demo/util/EnumUtil.java
@@ -0,0 +1,22 @@
+package com.example.demo.util;
+
+public class EnumUtil {
+    /**
+     * @param enumType
+     * @param targetValue
+     * @param <E> 
+     * @return true if enumType contains targetValue
+     */
+    public static <E extends Enum<E>> boolean contains(Class<E> enumType, String targetValue) {
+        if (enumType == null || targetValue == null) {
+            return false;
+        }
+
+        for (E enumConstant : enumType.getEnumConstants()) {
+            if (enumConstant.toString().equals(targetValue)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}

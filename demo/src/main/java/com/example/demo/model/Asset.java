347282cacba119032e611fc8bf61af32675fe89b
Panda77 <PiperZzz@users.noreply.github.com>
2023-07-25 18:55:05 -0400

    enum init

---
+++ demo/src/main/java/com/example/demo/model/Asset.java
@@ -0,0 +1,32 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.example.demo.enums.Symbol;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Asset {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_id", nullable = false)
+    private Wallet wallet;
+
+    @Enumerated(EnumType.STRING)
+    private Symbol symbol;
+
+    private double amount;
+    private String address;
+}

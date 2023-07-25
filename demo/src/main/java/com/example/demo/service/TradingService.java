a2152f802ee9b3f1e93b051893308a2119ccb5bb
Panda77 <PiperZzz@users.noreply.github.com>
2023-07-25 18:57:46 -0400

    update buy sell service

--- demo/src/main/java/com/example/demo/service/TradingService.java
+++ demo/src/main/java/com/example/demo/service/TradingService.java
@@ -1,27 +1,77 @@
 package com.example.demo.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.User;
+import com.example.demo.model.Wallet;
+import com.example.demo.enums.OrderStatus;
+import com.example.demo.enums.OrderType;
+import com.example.demo.enums.Symbol;
+import com.example.demo.model.Asset;
+import com.example.demo.model.Order;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.WalletRepository;
+import com.example.demo.repository.AssetRepository;
+import com.example.demo.repository.OrderRepository;
 
 @Service
 public class TradingService {
     private static final Logger logger= LoggerFactory.getLogger(TradingService.class);
 
     private final UserRepository userRepository;
+    private final WalletRepository walletRepository;
+    private final OrderRepository orderRepository;
+    private final AssetRepository assetRepository;
 
-    public TradingService(UserRepository userRepository) {
+    public TradingService(UserRepository userRepository, WalletRepository walletRepository, OrderRepository orderRepository, AssetRepository assetRepository) {
         this.userRepository = userRepository;
+        this.walletRepository = walletRepository;
+        this.orderRepository = orderRepository;
+        this.assetRepository = assetRepository;
     }
 
-    public void buy(String symbol, int amount) {
-        logger.info("Buying {} amount of {}", amount, symbol);
+    public void buy(User user, String symbol, double amount, double price) {
+        logger.info("User {} Buying {} Amount of {} at Price {}", user, amount, symbol, price);
+
+        Wallet wallet = walletRepository.findByUser(user);
+        if (wallet.getBalance() < amount * price) {
+            throw new RuntimeException("Insufficient balance");
+        }
+        wallet.setBalance(wallet.getBalance() - amount * price);
+        walletRepository.save(wallet);
+
+        Order order = new Order();
+        order.setUser(user);
+        order.setOrderType(OrderType.BUY);
+        order.setSymbol(Symbol.valueOf(symbol));
+        order.setAmount(amount);
+        order.setOrderStatus(OrderStatus.OPEN);
+        order.setOpenTime(LocalDateTime.now());
+        orderRepository.save(order);
     }
 
-    public void sell(String symbol, int amount) {
-        logger.info("Selling {} amount of {}", amount, symbol);
+    public void sell(User user, String symbol, double amount, double price) {
+        logger.info("User {} Selling {} Amount of {} at Price {}", user, amount, symbol, price);
+
+        Wallet wallet = walletRepository.findByUser(user);
+        List<Asset> assets = assetRepository.findBySymbol(wallet);
+        Optional<Asset> result = assets.stream().filter(asset -> asset.getSymbol().equals(Symbol.valueOf(symbol))).findFirst();
+        if (result.isPresent()) {
+            Asset asset = result.get();
+            if (asset.getAmount() < amount) {
+                throw new RuntimeException("Insufficient balance");
+            }
+            asset.setAmount(asset.getAmount() - amount);
+            assetRepository.save(asset);
+        } else {
+            throw new RuntimeException("Insufficient balance");
+        }
     }
 
     public void hold(String symbol, int amount) {

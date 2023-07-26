7a44b822e88383d939419a4ccd14a5caa8195523
Panda77 <PiperZzz@users.noreply.github.com>
2023-07-25 23:08:19 -0400

    specify orderType

--- demo/src/main/java/com/example/demo/enums/OrderType.java
+++ demo/src/main/java/com/example/demo/enums/OrderType.java
@@ -2,5 +2,11 @@ package com.example.demo.enums;
 
 public enum OrderType {
     BUY,
-    SELL;
+    BUY_LIMIT,
+    SELL,
+    SELL_LIMIT,
+    STOP_BUY,
+    STOP_BUY_LIMIT,
+    STOP_SELL,
+    STOP_SELL_LIMIT,
 }

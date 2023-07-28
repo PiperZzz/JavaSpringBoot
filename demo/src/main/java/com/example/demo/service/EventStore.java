6608a1e1e4763550daf27623bbdc5b97ff30684b
Panda77 <PiperZzz@users.noreply.github.com>
2023-07-28 10:48:25 -0400

    EventStore init

--- demo/src/main/java/com/example/demo/service/EventStore.java
+++ demo/src/main/java/com/example/demo/service/EventStore.java
@@ -1,19 +1,23 @@
-package com.example.demo.store;
+package com.example.demo.service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import com.example.demo.bo.OrderEvent;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EventStore {
-    // This could be a database, a file, a message queue, etc.
     private List<OrderEvent> orderEvents = new ArrayList<>();
 
     public void saveEvent(OrderEvent orderEvent) {
+        //TODO Save the event to database
         orderEvents.add(orderEvent);
     }
 
     public List<OrderEvent> getOrderEvents() {
+        //TODO Load the events from database
         return orderEvents;
     }
 }

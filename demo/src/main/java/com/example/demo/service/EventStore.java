49593fd0250eec4207a9e0a924a7ad99b0f93d71
Panda77 <PiperZzz@users.noreply.github.com>
2023-08-02 20:27:44 -0400

    kafka init message

--- demo/src/main/java/com/example/demo/service/EventStore.java
+++ demo/src/main/java/com/example/demo/service/EventStore.java
@@ -3,6 +3,8 @@ package com.example.demo.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import com.example.demo.bo.OrderEvent;
 
 import org.springframework.kafka.core.KafkaTemplate;
@@ -15,6 +17,11 @@ public class EventStore {
 
     public EventStore(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostConstruct
+    public void eventStoreInit() {
+        kafkaTemplate.send("order-events", "OrderStore Initialized");
     }
 
     public void saveEvent(OrderEvent orderEvent) {

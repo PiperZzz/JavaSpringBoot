4c9556c1919a73826032a6e764c756b219239165
Panda77 <PiperZzz@users.noreply.github.com>
2023-08-02 20:50:51 -0400

    enum topic

--- demo/src/main/java/com/example/demo/service/EventStore.java
+++ demo/src/main/java/com/example/demo/service/EventStore.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import com.example.demo.bo.OrderEvent;
+import com.example.demo.enums.MessageTopic;
 
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,7 @@ public class EventStore {
 
     @PostConstruct
     public void eventStoreInit() {
-        kafkaTemplate.send("order-events", "OrderStore Initialized");
+        kafkaTemplate.send(MessageTopic.ORDER_EVENT.name(), "OrderStore Initialized");
     }
 
     public void saveEvent(OrderEvent orderEvent) {

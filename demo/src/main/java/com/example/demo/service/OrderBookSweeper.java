85ab2bc09e33c9f0c16689d51327a141135a67fb
Panda77 <PiperZzz@users.noreply.github.com>
2023-08-21 12:10:39 -0400

    @Scheduled impl

--- demo/src/main/java/com/example/demo/service/OrderBookSweeper.java
+++ demo/src/main/java/com/example/demo/service/OrderBookSweeper.java
@@ -1,9 +1,8 @@
 package com.example.demo.service;
 
 import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.bo.OrderBook;
@@ -11,25 +10,12 @@ import com.example.demo.enums.SymbolCode;
 
 @Service
 public class OrderBookSweeper {
-    private Timer timer;
 
-    public OrderBookSweeper() {
-        timer = new Timer();
-        scheduleOrderCleanupTask();
-    }
-
-    private void scheduleOrderCleanupTask() {
-        TimerTask cleanupTask = new OrderCleanupTask();
-        timer.scheduleAtFixedRate(cleanupTask, 0, 1000);
-    }
-
-    private class OrderCleanupTask extends TimerTask {
-        @Override
-        public void run() {
-            Map<SymbolCode, OrderBook> orderBooks = OrderBookManager.getOrderBooks();
-            for (OrderBook orderBook : orderBooks.values()) {
-                orderBook.cleanupExpiredOrders();
-            }
+    @Scheduled(fixedRate = 1000)
+    public void cleanupExpiredOrders() {
+        Map<SymbolCode, OrderBook> orderBooks = OrderBookManager.getOrderBooks();
+        for (OrderBook orderBook : orderBooks.values()) {
+            orderBook.cleanupExpiredOrders();
         }
     }
 
